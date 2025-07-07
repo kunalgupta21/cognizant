6153ebf3577df979ee8779f9f29dad629f531ddf
Kunal Gupta <kunal2004.gupta@gmail.com>
2025-07-07 10:56:53 +0530

    added Spring Exercises

---
+++ week3/Implement_services_for_managing_Country/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,88 @@
+package com.cognizant.orm_learn;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static CountryService countryService;
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
+
+//		testGetAllCountries();
+//		testAddCountry();
+//		testUpdateCountry();
+		testDeleteCountry();
+	}
+
+
+	private static void testGetAllCountries() {
+		System.out.println("Start");
+		List<Country> countries = countryService.getAllCountries();
+		for (Country country : countries) {
+			System.out.println("Code = " + country.getCode() + ", Name = " + country.getName());
+		}
+		System.out.println("End");
+	}
+
+	private static void testAddCountry() {
+		System.out.println("Start");
+
+		Country country = new Country();
+		country.setCode("ZZ");
+		country.setName("Zootopia");
+
+		countryService.addCountry(country);
+
+		Country fetched = countryService.findCountryByCode("ZZ");
+
+		if (fetched != null) {
+			System.out.println("Country added: Code = " + fetched.getCode() + ", Name = " + fetched.getName());
+		} else {
+			System.out.println("Country not found");
+		}
+
+		System.out.println("End");
+	}
+
+	private static void testUpdateCountry() {
+		System.out.println("Start");
+
+		countryService.updateCountry("IN", "Bharat");
+
+		Country updated = countryService.findCountryByCode("IN");
+
+		if (updated != null) {
+			System.out.println("Updated Country: Code = " + updated.getCode() + ", Name = " + updated.getName());
+		} else {
+			System.out.println("Country not found");
+		}
+
+		System.out.println("End");
+	}
+
+	private static void testDeleteCountry() {
+		System.out.println("Start");
+
+		countryService.deleteCountry("ZZ"); // This assumes ZZ was added in previous test
+
+		Country deleted = countryService.findCountryByCode("ZZ");
+
+		if (deleted == null) {
+			System.out.println("Country with code ZZ has been deleted.");
+		} else {
+			System.out.println("Country still exists: Code = " + deleted.getCode() + ", Name = " + deleted.getName());
+		}
+
+		System.out.println("End");
+	}
+
+}
