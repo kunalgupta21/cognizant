6fbb3dbc8776b788fd8b7d587b35d1d576d30a48
Kunal Gupta <kunal2004.gupta@gmail.com>
2025-07-21 09:50:55 +0530

    Added week 5 handson

---
+++ week5/microservices/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.cognizant.account.controller;
+
+import com.cognizant.account.model.Account;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Account getAccountByNumber(@PathVariable String number) {
+        return new Account(number, "savings", 234343);
+    }
+}
---
+++ week5/microservices/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,14 @@
+package com.cognizant.loan.controller;
+
+import com.cognizant.loan.model.Loan;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Loan getLoanByNumber(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
+}
